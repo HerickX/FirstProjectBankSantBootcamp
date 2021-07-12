ad3940ac2ecc826bd2b3892df7339c30fd4ad606
perkles <htlh23901@gmail.com>
2021-07-12 20:22:42 -0300

    atualized person class

---
+++ src/main/kotlin/oneDigitalInnovationPJT/Pessoa.kt
@@ -0,0 +1,15 @@
+package oneDigitalInnovationPJT
+
+class Pessoa {
+    var nome: String = "Herick"
+    var cpf: String = "123.456.789.11"
+}
+
+
+fun main() {
+  val Herick =  Pessoa()
+
+    println(Herick.nome)
+    println(Herick.cpf)
+
+}
